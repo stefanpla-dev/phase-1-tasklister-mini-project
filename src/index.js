616bd2bda962f7a5c69d7c08cdfb9977a09a06e3
Stefan Pla <spla@icloud.com>
2024-02-09 10:50:32 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const list = document.querySelector("#tasks")
+  
+  const addSubmitListener = () => {
+    
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      let newTask = {
+        "new-task-description": e.target.querySelector("#new-task-description").value,
+      }
+      console.log(newTask)
+      let li = document.createElement("li")
+      li.textContent = newTask["new-task-description"];
+      list.append(li)
+    });
+  }
+  addSubmitListener();
 });
